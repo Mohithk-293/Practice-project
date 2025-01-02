c5ac0ad8e16114dc1be7006b74af09a31eca14fa
Mohith <mohithk487@gmail.com>
2025-01-02 16:10:15 +0530

    user route established

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const connect=require('../database')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  
+  connect.query("select * from Student",(err,results)=>{
+    if (err) {
+      console.log(err)
+      res.status(500).send('Error fetching data');
+      return;
+    }else{
+      res.status(200).json(results);
+    }
+  })
+ 
 });
 
 module.exports = router;
